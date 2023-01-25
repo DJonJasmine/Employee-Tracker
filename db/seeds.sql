d2878f8a0effc31477cc3beff7b2bb0aefc152a4
unknown <harrisondjon1994@gmail.com>
2023-01-25 14:41:13 -0800

    added code to server.js file and seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,28 @@
 INSERT INTO department (department_name)
-VALUES ("Sales"),
-       ("Accounting"),
-       ("Human Resources"),
-       ("Business Integrations");
+VALUES  ('Operations'),
+        ('Analystics'),
+        ('Marketing'),
+        ('Executive');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('General Manager', 80000, 1),
+       ('Team Lead Analyst', 70000, 1),
+       ('Sales Representative', 35000, 1),
+       ('Team Analyst', 75000, 2),
+       ('Media Manager', 65000, 2),
+       ('Media Specialist', 30000, 2),
+       ('HR Manager', 70000, 3),
+       ('HR Representative', 25000, 3),
+       ('CEO', 45000000, 4);
+       
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Freedy', 'Kruger', 1, NULL),
+       ('Leather', 'Face', 2, 1),
+       ('Hannibal', 'Lecter', 3, 1),
+       ('Jason', 'Voorhees', 4, NULL),
+       ('John', 'Kramer', 5, 4),
+       ('Michael', 'Meyers', 6, 4),
+       ('Penny', 'Wise', 7, NULL),
+       ('Norman', 'Bates', 8, 7);
+       
