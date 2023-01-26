d87e57ffcb4e71515d5ecbc0fb45318ddfaf0e2f
unknown <harrisondjon1994@gmail.com>
2023-01-26 15:58:00 -0800

    created new staff file and moved around code, created code in server.js

--- server.js
+++ server.js
@@ -1,96 +1,24 @@
-const inquirer = require('inquirer');
-require('console.table');
-// port
+const express = require("express");
+const db = require("./db/connection");
+const inquirer = require("./lib/staff");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+// express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-// prompt questions
-const promptMessages = () => {
-  return inquirer.prompt ([
-    {
-      viewAllEmployees: 'View All Employees',
-      viewByDepartment: 'View All Employees By Department',
-      viewByRole: 'View All Employees By Role',
-      viewByManager: 'View All Employees By Manager',
-      addEmployee: 'Add An Employee',
-      addRole: 'Add Role',
-      updateEmployee: 'Update An Employee',
-      updateEmployeeRole: 'Update An Employee Role',
-      updateEmployeeManager: 'Update  Employee Manager',
-      removeEmployee: 'Remove An Employee',
-      removeEmployeeRole: 'Remove An Employee Role',
-      removeEmployeeManager: 'Remove An Employee Manager',
-      removeDepartment: 'Remove An Department',
-      exit: 'Exit'
-    }
-    
-  ]).then((data) => {
-    switch (data.selection) {
-        case 'View All Employees':
-            viewAllEmployees();
-            break;
+// Default response for any other request (Not found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-        case 'View All Employees By Department':
-            viewByDepartment();
-            break;
-            
-        case 'View All Employees By Role':
-            viewByRole();
-            break;
-        
-        case 'View All Employees By Manager':
-            viewByManager();
-            break;
-    
-        case 'Add An Employee':
-            addEmployee();
-            break;
-        
-        case 'Add Role':
-            addRole();
-            break;
-            
-        case 'Update An Employee':
-            updateEmployee();
-            break;
-       
-        case 'Update An Employee Role':
-            updateEmployeeRole();
-            break;
-        
-        case 'Update An Employee Manager':
-              updateEmployeeManager();
-              break;
-              
-        case 'Remove An Employee':
-              removeEmployee();
-              break;
-                
-        case 'Remove An Employee Role':
-              removeEmployeeRole();
-              break;
-
-        case 'Remove An Employee Manager':
-              removeEmployeeManager();
-              break;
-                    
-        case 'Remove An Department':
-              removeDepartment();
-              break;
-                      
-        case 'Exit':
-              exit();
-              break;
-            
-    }
-}) 
-};
-promptMessages();
-
-const viewAllEmployees = () => {
-  db.query('SELECT * FROM department', function (err, results) {
-    console.table(results);
-    console.log(`\n`);
-    promptMessages();
-  })
-}
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database successfully connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    inquirer();
+  });
+});
